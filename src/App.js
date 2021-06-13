42bd120a2fa534af32e6a19694223cddc527d26f
LauraBraun <77805783+LauraBraun@users.noreply.github.com>
2021-06-13 21:39:43 +0200

    added github repo url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
      Weather App
+     <footer>This project was coded by Laura Braun and is <a href="https://github.com/LauraBraun/weather-react-app" target="_blank" rel="noreferrer">open-sourced</a> on github.</footer>
     </div>
   );
 }
