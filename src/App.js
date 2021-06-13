5c92383dce343e36e8480c628b2879706088f72e
LauraBraun <77805783+LauraBraun@users.noreply.github.com>
2021-06-13 21:57:16 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
      Weather App
      <footer>This project was coded by Laura Braun and is <a href="https://github.com/LauraBraun/weather-react-app" target="_blank" rel="noreferrer">open-sourced</a> on github.</footer>
+    </div>
     </div>
   );
 }
