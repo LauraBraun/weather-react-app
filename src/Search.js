f0520cb7669e790f6d29b3234751e3aed1c0dd13
LauraBraun <77805783+LauraBraun@users.noreply.github.com>
2021-06-13 23:13:57 +0200

    add api

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState} from "react";
+import axios from "axios";
 import "./Search.css";
 
 export default function Search(){
-    return (
+    const [weather, setWeather] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data)
+    setWeather({
+      ready: true,
+      temp: response.data.main.temp,
+      city: response.data.name,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity
+    })
+  }
+ 
+  if (weather.ready) {
+  return (
     <div className="Search"> 
         <div className="row">
           <div className="col-sm">
@@ -29,21 +45,21 @@ export default function Search(){
               <button className="btn btn-outline-secondary" id="coordinates">
                 Current location
               </button>
-              <img src="" alt="weather description" id="currentConditions" />
-              <span className="currentTemp">20</span>
+              <img src="" alt={weather.description} id="currentConditions" />
+              <span className="currentTemp">{Math.round(weather.temp)}</span>
               <button id="degreeConversion">°C</button>
             </section>
           </div>
           <div className="col">
-            <h2 id="displayCity">Amsterdam</h2>
+            <h2 id="displayCity">{weather.city}</h2>
             <h2 id="currentDay">Thursday</h2>
             <span id="currentDate">10:30</span>
             <br />
             <br />
-            <div id="weatherDescription">Description</div>
-            <div id="humidity">Humidity:</div>
-            <div id="windspeed">Wind: m/s</div>
-            <div id="precipitation">Precipitation</div>
+            <div id="weatherDescription" className="text-capitalize">{weather.description}</div>
+            <div id="humidity">Humidity:{weather.humidity}</div>
+            <div id="windspeed">Wind: {weather.wind} m/s</div>
+            <div id="precipitation">Precipitation: 20%</div>
           </div>
         </div>
 
@@ -53,4 +69,13 @@ export default function Search(){
         <div className="row" id="forecast"></div>
       </footer>
       </div>);
+  } else {
+     let city ="Amsterdam"
+  let apiKey=`2312e7899c189d46fb63d2d7ce28c492`;
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return "Loading...."
+
+  }
 }
